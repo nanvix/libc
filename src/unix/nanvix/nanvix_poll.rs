6de153fca27f0f4ea889ea2f343d198c9df3aa78
Pedro Henrique Penna <ppenna@microsoft.com>
2025-06-19 18:45:47 -0700

    Dummy POLL Options

--- src/unix/nanvix/nanvix_poll.rs
+++ src/unix/nanvix/nanvix_poll.rs
@@ -1,2 +1,15 @@
+use std::os::nanvix::ffi::c_short;
+
 pub use std::os::nanvix::syscall::sysapi::poll::nfds_t;
 pub use std::os::nanvix::syscall::sysapi::poll::pollfd;
+
+pub const POLLIN: c_short = 0x001;
+pub const POLLPRI: c_short = 0x002;
+pub const POLLOUT: c_short = 0x004;
+pub const POLLERR: c_short = 0x008;
+pub const POLLHUP: c_short = 0x010;
+pub const POLLNVAL: c_short = 0x020;
+pub const POLLRDNORM: c_short = 0x040;
+pub const POLLRDBAND: c_short = 0x080;
+pub const POLLWRNORM: c_short = 0x100;
+pub const POLLWRBAND: c_short = 0x200;
