b99dbe6313925568d2a8d07b1ca6437b8c879146
danbugs <danilochiarlone@gmail.com>
2025-07-04 22:25:08 +0000

    fixup! [tmp] remove missing import from nanvix sysapi
    Signed-off-by: danbugs <danilochiarlone@gmail.com>

--- src/unix/nanvix/nanvix_fcntl.rs
+++ src/unix/nanvix/nanvix_fcntl.rs
@@ -8,7 +8,7 @@ pub const LOCK_UN: c_int = 8;  // Unlock
 
 // from: i686-nanvix/include/sys/errno.h
 pub const EAGAIN: c_int = 11;
-pub const EWOULDBLOCK: c_int = EAGAIN;
+pub const EWOULDBLOCK: c_int = 11;
 
 pub use std::os::nanvix::syscall::sysapi::fcntl::atflags::*;
 pub use std::os::nanvix::syscall::sysapi::fcntl::file_access_mode::*;
