ab0c88aea29a91c9f5a66c077e9c10da100a0d03
danbugs <danilochiarlone@gmail.com>
2025-07-29 18:41:08 +0000

    fixup! include all in unistd
    Signed-off-by: danbugs <danilochiarlone@gmail.com>

--- src/unix/nanvix/nanvix_unistd.rs
+++ src/unix/nanvix/nanvix_unistd.rs
@@ -1,1 +1,6 @@
-pub use std::os::nanvix::syscall::sysapi::unistd::*;
+pub use std::os::nanvix::syscall::sysapi::unistd::file_seek::*;
+pub use std::os::nanvix::syscall::sysapi::unistd::getentropy;
+pub use std::os::nanvix::syscall::sysapi::unistd::read;
+pub use std::os::nanvix::syscall::sysapi::unistd::STDERR_FILENO;
+pub use std::os::nanvix::syscall::sysapi::unistd::STDIN_FILENO;
+pub use std::os::nanvix::syscall::sysapi::unistd::STDOUT_FILENO;
