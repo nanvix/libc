87fbecbfbe5d870f007dbf77dd46c17197b29384
Pedro Henrique Penna <ppenna@microsoft.com>
2025-08-14 17:33:17 -0700

    Adding Exports

--- src/unix/nanvix/nanvix_tmp.rs
+++ src/unix/nanvix/nanvix_tmp.rs
@@ -1,8 +1,29 @@
 #![allow(unused_imports)]
-pub use std::os::nanvix::syscall::stdlib::*;
+pub use std::os::nanvix::syscall::pthread::bindings::{
+    pthread_attr_destroy::pthread_attr_destroy, pthread_attr_init::pthread_attr_init,
+    pthread_attr_setstacksize::pthread_attr_setstacksize,
+    pthread_cond_broadcast::pthread_cond_broadcast, pthread_cond_destroy::pthread_cond_destroy,
+    pthread_cond_init::pthread_cond_init, pthread_cond_signal::pthread_cond_signal,
+    pthread_cond_timedwait::pthread_cond_timedwait, pthread_cond_wait::pthread_cond_wait,
+    pthread_create::pthread_create, pthread_getschedparam::pthread_getschedparam,
+    pthread_getspecific::pthread_getspecific, pthread_join::pthread_join,
+    pthread_key_create::pthread_key_create, pthread_kill::pthread_kill,
+    pthread_mutex_destroy::pthread_mutex_destroy, pthread_mutex_init::pthread_mutex_init,
+    pthread_mutex_lock::pthread_mutex_lock, pthread_mutex_unlock::pthread_mutex_unlock,
+    pthread_rwlock_rdlock::pthread_rwlock_rdlock, pthread_rwlock_unlock::pthread_rwlock_unlock,
+    pthread_rwlock_wrlock::pthread_rwlock_wrlock, pthread_self::pthread_self,
+    pthread_setcancelstate::pthread_setcancelstate, pthread_setspecific::pthread_setspecific,
+    pthread_sigmask::pthread_sigmask, sem_destroy::sem_destroy, sem_init::sem_init,
+    sem_post::sem_post, sem_wait::sem_wait,
+};
+pub use std::os::nanvix::syscall::sched::bindings::sched_yield::sched_yield;
+pub use std::os::nanvix::syscall::signal::bindings::{kill::kill, sigaction::sigaction};
+pub use std::os::nanvix::syscall::stdlib::bindings::{
+    aligned_alloc::{__aligned_free, __aligned_malloc},
+    posix_memalign::posix_memalign,
+};
+pub use std::os::nanvix::syscall::sys::mman::bindings::{
+    mmap::mmap, mprotect::mprotect, munmap::munmap,
+};
+pub use std::os::nanvix::syscall::sys::stat::bindings::{fstat::fstat, stat::stat};
 pub use std::os::nanvix::syscall::time::bindings::*;
-pub use std::os::nanvix::syscall::pthread::bindings::*;
-pub use std::os::nanvix::syscall::sched::bindings::*;
-pub use std::os::nanvix::syscall::sys::stat::bindings::*;
-pub use std::os::nanvix::syscall::sys::{mmap, munmap, mprotect};
-pub use std::os::nanvix::syscall::signal::{sigaction, kill};
