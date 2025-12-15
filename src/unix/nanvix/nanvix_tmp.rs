5ae063dcbfce2b09d51b2f00aebcd65443b2f2b2
Pedro Henrique Penna <ppenna@microsoft.com>
2025-12-15 12:19:39 -0800

    Fix Build

--- src/unix/nanvix/nanvix_tmp.rs
+++ src/unix/nanvix/nanvix_tmp.rs
@@ -5,7 +5,6 @@ pub use std::os::nanvix::syscall::pthread::bindings::{
     pthread_cond_broadcast::pthread_cond_broadcast, pthread_cond_destroy::pthread_cond_destroy,
     pthread_cond_init::pthread_cond_init, pthread_cond_signal::pthread_cond_signal,
     pthread_cond_timedwait::pthread_cond_timedwait, pthread_cond_wait::pthread_cond_wait,
-    pthread_condattr_destroy::pthread_condattr_destroy,
     pthread_condattr_init::pthread_condattr_init,
     pthread_condattr_setclock::pthread_condattr_setclock, pthread_create::pthread_create,
     pthread_getschedparam::pthread_getschedparam, pthread_getspecific::pthread_getspecific,
